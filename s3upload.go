e3219285ce2c3debf9e13c2880538972b84e8db0
greenac <andre1226.dev@gmail.com>
2018-04-18 14:50:53 -0700

    added .env file

--- s3upload.go
+++ s3upload.go
@@ -1,7 +1,36 @@
 package main
 
-import "github.com/greenac/s3upload/file"
+import (
+	"github.com/greenac/s3upload/file"
+	"github.com/joho/godotenv"
+	"github.com/greenac/s3upload/logger"
+	"os"
+)
 
 func main() {
-	file.GetFiles("/Users/andre/vbshare/cards", "cards.dev.longgame.co")
+	err := godotenv.Load()
+	if err != nil {
+		logger.Error("Error loading .env file")
+	}
+
+	ul := os.Getenv("USE_LOCAL")
+	if ul == "true" {
+		bp := os.Getenv("BASE_PATH")
+		tp := os.Getenv("TARGET_PATH")
+		if bp == "" || tp == "" {
+			logger.Error("BASE_PATH and/or TARGET_PATH must be set in .env file")
+			panic("Missing Env Variable")
+		}
+
+		file.GetFilesLocal(bp, tp)
+	} else {
+		bp := os.Getenv("BASE_PATH")
+		bkp := os.Getenv("BUCKET")
+		if bp == "" || bkp == "" {
+			logger.Error("BASE_PATH and/or BUCKET must be set in .env file")
+			panic("Missing Env Variable")
+		}
+
+		file.GetFiles(bp, bkp)
+	}
 }
